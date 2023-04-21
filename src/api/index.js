2173fb1cd1d4bd0950b3ad2007486d0dec7e1326
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-04-21 19:27:13 +0200

    change baseUrl

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  // baseURL: 'https://petsitterfriendly-production.up.railway.app/',
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://petsitterfriendly-production.up.railway.app/',
+  // baseURL: 'http://localhost:3001/',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
   },
